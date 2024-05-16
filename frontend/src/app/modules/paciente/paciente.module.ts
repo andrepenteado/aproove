952c5d1cd9d18806956d011b2c5ff70835e2b9ab
André Penteado <andre.penteado@unesp.br>
2024-05-16 18:05:01 -0300

    Mudança da coluna ações dos grids

--- frontend/src/app/modules/paciente/paciente.module.ts
+++ frontend/src/app/modules/paciente/paciente.module.ts
@@ -2,13 +2,12 @@ import { CommonModule, DatePipe } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
 
+import { NgxApcoreModule } from "@andrepenteado/ngx-apcore";
+import { ReactiveFormsModule } from "@angular/forms";
+import { NgxLoadingModule } from "ngx-loading";
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { PacienteRoutingModule } from './paciente-routing.module';
 import { PesquisarComponent } from './pesquisar/pesquisar.component';
-import { DataTablesModule } from 'angular-datatables';
-import { NgxApcoreModule } from "@andrepenteado/ngx-apcore";
-import { NgxLoadingModule } from "ngx-loading";
-import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -20,7 +19,6 @@ import { ReactiveFormsModule } from "@angular/forms";
     PacienteRoutingModule,
     NgxMaskDirective,
     NgxMaskPipe,
-    DataTablesModule,
     NgxApcoreModule,
     NgxLoadingModule,
     ReactiveFormsModule
