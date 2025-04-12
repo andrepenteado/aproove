6d08ce5622b2f01ebcf16179d27536ffdf4b4455
André Penteado <andre.penteado@unesp.br>
2025-04-11 22:08:19 -0300

    Permissão de menus por perfil

--- frontend/src/app/config/menu.ts
+++ frontend/src/app/config/menu.ts
@@ -1,11 +1,12 @@
 import { Menu } from "@andre.penteado/ngx-apcore";
+import { PREFIXO_PERFIL_SISTEMA } from "./layout";
 
 export const MENU: Menu[] = [
   {
-    id: "dashboard", texto: "Dashboard", icone: "chart-line", path: "/dashboard", subMenus: []
+    id: "dashboard", texto: "Dashboard", icone: "chart-line", path: "/dashboard", roles: [`${PREFIXO_PERFIL_SISTEMA}FISIOTERAPEUTA`], subMenus: []
   },
   {
-    id: "paciente", texto: "Paciente", icone: "hospital-user", path: "/paciente/pesquisar", subMenus: []
+    id: "paciente", texto: "Paciente", icone: "hospital-user", path: "/paciente/pesquisar", roles: [`${PREFIXO_PERFIL_SISTEMA}FISIOTERAPEUTA`], subMenus: []
   }
 
 ];
